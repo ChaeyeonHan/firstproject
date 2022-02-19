09a42dd084b253e96f440c6c19864fb3eebfbec9
ChaeyeonHan <kate2236e@naver.com>
2022-02-19 23:35:47 +0900

    로깅설정

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,20 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity  // DB가 해당 객체를 인식가능
+@Entity  // DB가 해당 객체를 인식가능 + (해당 클래스로 table을 만든다)
 @AllArgsConstructor
 @NoArgsConstructor  // 디폴트 생성자 추가!
 @ToString
 @Getter  // getId()를 사용하기위해 추가
 public class Article {
 
-    @Id // 대표값을 지정한다(주민번호 같은것.) 제목가 내용이 동일할때 구분하기 위함
-    @GeneratedValue  // 자동생성해주는 어노테이션. 1, 2, 3, ...
+    @Id // 대표값을 지정한다(주민등록번호 같은것.) 제목가 내용이 동일할때 구분하기 위함
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // DB가 id를 자동생성하는 어노테이션
     private Long id;
 
    @Column  // DB에서 관리하는 table에 연결되게 해준다
