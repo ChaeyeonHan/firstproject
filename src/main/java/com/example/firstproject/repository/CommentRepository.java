e0eccd9f16d5433af41f37ca4f91e76d0edc9304
ChaeyeonHan <kate2236e@naver.com>
2022-02-22 11:09:37 +0900

    에러해결

--- src/main/java/com/example/firstproject/repository/CommentRepository.java
+++ src/main/java/com/example/firstproject/repository/CommentRepository.java
@@ -4,6 +4,7 @@ import com.example.firstproject.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,9 +13,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query(value =
             "SELECT *" +
             " FROM comment" +
-            " WHERE article_id = : articleId",
+            " WHERE article_id = :articleId",
             nativeQuery = true)
-    List<Comment> findByArticleId(Long articleId);
+    List<Comment> findByArticleId(@Param("articleId") Long articleId);
 
     // 특정 닉네임의 모든 댓글 조회
     List<Comment> findByNickname(String nickname);
